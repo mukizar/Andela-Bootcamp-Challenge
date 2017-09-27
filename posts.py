e83d916500237896e117be567b829718596f0eba
mukizar <rmukiza@gmail.com>
2017-09-27 09:29:13 +0300

    update FlaskApp files

---
+++ posts.py
@@ -0,0 +1,21 @@
+def Posts():
+    posts = [
+        {
+
+            'id':1,
+            'title': 'How to make a rolex',
+            'body': 'A mug or cup  must be used to mix the ingredients together.Second, the egg mixture must be poured onto a hot, well-oiled pan and spread out with the same spoon used to whip the eggs.Second, the egg mixture must be poured onto a hot, well-oiled pan and spread out with the same spoon used to whip the eggs.Second, the egg mixture must be poured onto a hot, well-oiled pan and spread out with the same spoon used to whip the eggs.Second, the egg mixture must be poured onto a hot, well-oiled pan and spread out with the same spoon used to whip the eggs. Second, the egg mixture must be poured onto a hot, well-oiled pan and spread out with the same spoon used to whip the eggs. Second, the egg mixture must be poured onto a hot, well-oiled pan and spread out with the same spoon used to whip the eggs.',
+            'author':'Mukiza Rayner',
+            'post_date':'26-09-2017'
+
+        },
+
+        {
+          'id':2,
+            'title': 'How to make a rolex',
+            'body': 'A mug or cup  must be used to mix the ingredients together.Second, the egg mixture must be poured onto a hot, well-oiled pan and spread out with the same spoon used to whip the eggs.Second, the egg mixture must be poured onto a hot, well-oiled pan and spread out with the same spoon used to whip the eggs.Second, the egg mixture must be poured onto a hot, well-oiled pan and spread out with the same spoon used to whip the eggs.Second, the egg mixture must be poured onto a hot, well-oiled pan and spread out with the same spoon used to whip the eggs. Second, the egg mixture must be poured onto a hot, well-oiled pan and spread out with the same spoon used to whip the eggs. Second, the egg mixture must be poured onto a hot, well-oiled pan and spread out with the same spoon used to whip the eggs.',
+            'author':'Bwengye Damiem',
+            'post_date':'26-09-2017'  
+
+        }
+    ]
