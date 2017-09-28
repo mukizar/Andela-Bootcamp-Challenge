17e2ce27a43221e7cb7f1e6293d91eb725a1c564
mukizar <rmukiza@gmail.com>
2017-09-28 10:31:39 +0300

    update to pylint requirements

--- app.py
+++ app.py
@@ -1,52 +1,54 @@
+"""Setting up flaskApp"""
 from flask import Flask, render_template, request
 from data import Posts
-from wtforms import Form, StringField, TextAreaField, PasswordField, validators
+from wtforms import Form, StringField, PasswordField, validators
 
-app = Flask(__name__)
+app = Flask(__name__) # pylint: disable=invalid-name
 
-Posts = Posts()
-
+POSTS = Posts()
 
 @app.route('/')
 def home():
+    """function to return home template"""
     return render_template('home.html')
-
-
-
 
 @app.route("/about")
 def about():
+    """function to return about template"""
     return render_template("about.html")
 
 @app.route("/posts")
 def posts():
-    return render_template("posts.html", posts= Posts)
+    """function to return posts template"""
+    return render_template("posts.html", POSTS=Posts)
 
 class SignUpForm(Form):
+    """Setting up wtform for SignUp"""
     name = StringField('Name', [validators.Length(min=1, max=50)])
     username = StringField('Username', [validators.Length(min=4, max=25)])
     email = StringField('Email', [validators.Length(min=6, max=50)])
-    password = PasswordField('Password',[
+    password = PasswordField('Password', [
         validators.DataRequired(),
         validators.EqualTo('confirm', message='Passwords do not match')
     ])
     confirm = PasswordField('Confirm Password')
 
-#User SignUp
 @app.route('/signup', methods=['GET', 'POST'])
-def SignUp():
+def sign_up():
+    """function for returning the SignUp page"""
     form = SignUpForm(request.form)
     if request.method == 'POST' and form.validate():
-        return render_template('signup.html') 
+        return render_template('signup.html')
     return render_template('signup.html', form=form)
 
 class SignInForm(Form):
+    """Setting up wtform for SignIn"""
     email = StringField('Email', [validators.Length(min=6, max=50)])
-    password = PasswordField('Password',[validators.DataRequired()])
+    password = PasswordField('Password', [validators.DataRequired()])
 
-# User Signin
 @app.route("/signin", methods=['GET', 'POST'])
-def signin():
+def sign_in():
+    """function to return SignIn page"""
     form = SignInForm(request.form)
     return render_template("signin.html", form=form)
 
