bd8fb03671af9fb6973272b036c0a0155c48ee89
mukizar <rmukiza@gmail.com>
2017-09-28 10:08:59 +0300

    test functionality of flaskapp

--- test.py
+++ test.py
@@ -1,24 +1,29 @@
-from app import app
+""" Create tests to check functionality and existence of pages"""
+
 import unittest
+from app import app
 
 
 class FlaskTestCase(unittest.TestCase):
+    """Defining the tests"""
 
-    #test if flask was setup correctly
-    def test_index(self):
-        #use tester to create test
+    def test_signin(self):
+        """function defining the test for the SignInpage """
         tester = app.test_client(self)
         response = tester.get('/signin', content_type='html/text')
         self.assertEqual(response.status_code, 200)
-    
-    # test if the signin page loads as designed
 
-
-    def test_signin_page_loads(self):
-        
+    def test_signup(self):
+        """function defining the test for the SignUpage """
         tester = app.test_client(self)
-        response = tester.get('/signin', content_type='html/text')
-        self.assertTrue(b'SignIn' in response.data)
+        response = tester.get('/signup', content_type='html/text')
+        self.assertEqual(response.status_code, 200)
+
+    def test_user_dashboard(self):
+        """function defining the test for the Postspage """
+        tester = app.test_client(self)
+        response = tester.get('/posts', content_type='html/text')
+        self.assertEqual(response.status_code, 200)
 
 
 if __name__ == '__main__':
